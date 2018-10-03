c2a2d673e916ac09fed212804d7473d099651107
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-04 01:53:58 +0530

    React API ~ createElement()

---
+++ course-1 React Basics/scoreboard/app.js
@@ -0,0 +1,7 @@
+const title = React.createElement(
+  "h1",
+  { id: "main-title", title: "This is a title." },
+  "My First React Element!"
+);
+
+ReactDOM.render(title, document.getElementById("root"));

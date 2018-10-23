7d5c328c991b5e6650ecb16b36d013312657cea3
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-24 01:01:51 +0530

    Component Composition

--- course-1 React Basics/scoreboard/app.js
+++ course-1 React Basics/scoreboard/app.js
@@ -1,14 +1,3 @@
-// function Header() {
-//   return (
-//     <header>
-//       <h1>Scoreboard</h1>
-//       <span className="stats">Players: 1</span>
-//     </header>
-//   );
-// }
-
-// Implicit Return with Arrow Functional Component ~ When we only returning some JSX within the component.
-
 const Header = () => (
   <header>
     <h1>Scoreboard</h1>
@@ -16,4 +5,32 @@ const Header = () => (
   </header>
 );
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Guil</span>
+      <Counter />
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+      <Player />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));

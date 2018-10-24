9cea60e95cc8f3f0610fd6efb46c1854cfd2d206
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-24 22:44:49 +0530

    Over 9000 ~ Inverse Data flow from Control Component Successfull

--- course-2 React Components Intermediate/src/components/App.js
+++ course-2 React Components Intermediate/src/components/App.js
@@ -7,19 +7,23 @@ class App extends React.Component {
     players: [
       {
         name: "Guil",
-        id: 1
+        id: 1,
+        score: 0
       },
       {
         name: "Treasure",
-        id: 2
+        id: 2,
+        score: 0
       },
       {
         name: "Ashley",
-        id: 3
+        id: 3,
+        score: 0
       },
       {
         name: "James",
-        id: 4
+        id: 4,
+        score: 0
       }
     ]
   };
@@ -32,18 +36,27 @@ class App extends React.Component {
     });
   };
 
+  handleChangeScore(index, delta) {
+    this.setState(prevState => {
+      score: prevState.players[index].score += delta;
+    });
+  }
+
   render() {
     return (
       <div className="scoreboard">
         <Header title="Scoreboard" totalPlayers={this.state.players.length} />
 
         {/* Players list */}
-        {this.state.players.map(player => (
+        {this.state.players.map((player, index) => (
           <Player
             name={player.name}
             id={player.id}
             key={player.id.toString()}
             removePlayer={this.handleRemovePlayer}
+            score={player.score}
+            changeScore={this.handleChangeScore}
+            index={index}
           />
         ))}
       </div>

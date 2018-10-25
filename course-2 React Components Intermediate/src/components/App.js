8f45e70b671e7b5df17cbf484fa1bb936d94561b
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-25 21:51:19 +0530

    Controlled Component in React ~ Basic Form Control

--- course-2 React Components Intermediate/src/components/App.js
+++ course-2 React Components Intermediate/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Player from "./Player";
+import AddPlayerForm from "./AddPlayerForm";
 
 class App extends React.Component {
   state = {
@@ -71,6 +72,8 @@ class App extends React.Component {
             removePlayer={this.handleRemovePlayer}
           />
         ))}
+
+        <AddPlayerForm />
       </div>
     );
   }

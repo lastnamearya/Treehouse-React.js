15a8800a2510261333bcfe86974bf9fdc3307fc8
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-28 22:03:43 +0530

    My solution ~ but state is rendering asynchronously

--- course-2 React Components Intermediate/src/components/Stopwatch.js
+++ course-2 React Components Intermediate/src/components/Stopwatch.js
@@ -6,23 +6,35 @@ export default class Stopwatch extends Component {
     time: 0
   };
 
-  toggleButton = () => {
+  handleStopwatch = () => {
+    this.setState({
+      isRunning: !this.state.isRunning
+    });
+    if (!this.state.isRunning) {
+      setInterval(this.tick, 1000);
+    }
+  };
+
+  tick = () => {
     this.setState(prevState => {
-      return {
-        isRunning: !prevState.isRunning
-      };
+      time: prevState.time += 1;
+      console.log(this.state.time);
     });
   };
 
-  render() {
-    var switchButton = this.state.isRunning ? "STOP" : "START";
+  reset = () => {
+    this.setState({ time: 0 });
+  };
 
+  render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">{this.state.time}</span>
-        <button onClick={this.toggleButton}>{switchButton}</button>
-        <button>Reset</button>
+        <button onClick={this.handleStopwatch}>
+          {this.state.isRunning ? "STOP" : "START"}
+        </button>
+        <button onClick={this.reset}>Reset</button>
       </div>
     );
   }

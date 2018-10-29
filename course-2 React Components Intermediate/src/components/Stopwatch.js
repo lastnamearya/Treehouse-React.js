7a20f28d22c3d570e9eb76d7a25af56f39ad4492
lastnamearya <arya.jigyasu6815@gmail.com>
2018-10-29 11:02:00 +0530

    Done with the Stopwatch component

--- course-2 React Components Intermediate/src/components/Stopwatch.js
+++ course-2 React Components Intermediate/src/components/Stopwatch.js
@@ -3,38 +3,50 @@ import React, { Component } from "react";
 export default class Stopwatch extends Component {
   state = {
     isRunning: false,
-    time: 0
+    elapsedtime: 0,
+    previousTime: 0
   };
 
-  handleStopwatch = () => {
-    this.setState({
-      isRunning: !this.state.isRunning
-    });
-    if (!this.state.isRunning) {
-      setInterval(this.tick, 1000);
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now();
+      this.setState(prevState => ({
+        previousTime: now,
+        elapsedtime: prevState.elapsedtime + (now - prevState.previousTime)
+      }));
     }
   };
 
-  tick = () => {
-    this.setState(prevState => {
-      time: prevState.time += 1;
-      console.log(this.state.time);
+  handleReset = () => {
+    this.setState({
+      elapsedtime: 0
     });
   };
 
-  reset = () => {
-    this.setState({ time: 0 });
+  handleStopwatch = () => {
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+    }
   };
 
   render() {
+    const seconds = Math.floor(this.state.elapsedtime / 1000);
+
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <span className="stopwatch-time">{this.state.time}</span>
+        <span className="stopwatch-time">{seconds}</span>
         <button onClick={this.handleStopwatch}>
           {this.state.isRunning ? "STOP" : "START"}
         </button>
-        <button onClick={this.reset}>Reset</button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
